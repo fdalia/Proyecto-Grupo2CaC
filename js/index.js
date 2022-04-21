dd1f60da7ce2d4f01f183ba4a42f51a0a83bbbd6
desnetag <dsaenger@hotmail.com>
2022-04-21 18:57:26 -0300

    Test 1

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+
+/* --------- Instancias Carousel------------*/
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false
+})
