34af0c248541159f087630a750f04572d2dcb87b
desnetag <dsaenger@hotmail.com>
2022-04-24 20:18:01 -0300

    Payment_Carousel

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/* --------- Instancias Carousel------------*/
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false
+})
+
