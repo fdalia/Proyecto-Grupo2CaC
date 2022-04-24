8b94d3d8074ac96e951eedadc7b4f79367210dfe
desnetag <dsaenger@hotmail.com>
2022-04-24 20:54:53 -0300

    Pay2
    More

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 /* --------- Instancias Carousel------------*/
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 2000,
-  wrap: false
+var myCarouselEl = document.getElementById('myCarousel')
+var carousel = bootstrap.Carousel.getInstance(myCarouselEl) // Retrieve a Carousel instance
+
+myCarouselEl.addEventListener('slid.bs.carousel', function (event) {
+  carousel.to('2') // Will slide to the slide 2 as soon as the transition to slide 1 is finished
 })
+
+carousel.to('1') // Will start sliding to the slide 1 and returns to the caller
+carousel.to('2') // !! Will be ignored, as the transition to the slide 1 is not finished !!
 
